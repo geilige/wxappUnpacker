3248bfc3c1c383c4054c2bf9b8e1b0a041e2f3d3
geilige <582102796@qq.com>
2019-11-18 21:36:01 +0800

    解决 $gwx is not defined 解决 __vd_version_info__ is not defined 解决ReferenceError: __mainPageFrameReady__ is not defined

--- wuWxss.js
+++ wuWxss.js
@@ -64,12 +64,31 @@ function doWxss(dir,cb){
 		};
 	}
 	function runVM(name,code){
-		let wxAppCode={},handle={cssFile:name};
-		let vm=new VM({sandbox:Object.assign(new GwxCfg(),{__wxAppCode__:wxAppCode,setCssToHead:cssRebuild.bind(handle)})});
+		// let wxAppCode={},handle={cssFile:name};
+		// let vm=new VM({sandbox:Object.assign(new GwxCfg(),{__wxAppCode__:wxAppCode,setCssToHead:cssRebuild.bind(handle)})});
+		// vm.run(code);
+		// for(let name in wxAppCode)if(name.endsWith(".wxss")){
+		// 	handle.cssFile=path.resolve(frameName,"..",name);
+		// 	wxAppCode[name]();
+		// }
+		let wxAppCode = {};
+		let handle = {cssFile: name};
+		let gg = new GwxCfg();
+		let tsandbox = {
+			$gwx: GwxCfg.prototype["$gwx"],
+			__mainPageFrameReady__: GwxCfg.prototype["$gwx"],   //解决 $gwx is not defined
+			__vd_version_info__: GwxCfg.prototype["$gwx"],  //解决 __vd_version_info__ is not defined
+			__wxAppCode__: wxAppCode,
+			setCssToHead: cssRebuild.bind(handle)
+		}
+
+		let vm = new VM({sandbox: tsandbox});
 		vm.run(code);
-		for(let name in wxAppCode)if(name.endsWith(".wxss")){
-			handle.cssFile=path.resolve(frameName,"..",name);
-			wxAppCode[name]();
+		for (let name in wxAppCode) {
+			if (name.endsWith(".wxss")) {
+				handle.cssFile = path.resolve(frameName, "..", name);
+				wxAppCode[name]();
+			}
 		}
 	}
 	function preRun(dir,frameFile,mainCode,files,cb){
